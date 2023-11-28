12c10ed0cb081a904c68cf5498ee9463acd96673
Lenanrd Boysen <57866677+LenBoysen@users.noreply.github.com>
2023-11-28 20:04:35 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,53 +1,40 @@
-function Show-AsciiGui {
+function Update-Date {
     param (
-        [string]$inputText
+        [int]$DelayMilliseconds
     )
 
-    $runspace = [runspacefactory]::CreateRunspace()
-    $runspace.ApartmentState = "STA"
-    $runspace.ThreadOptions = "ReuseThread"
-    $runspace.Open()
-    $runspace.SessionStateProxy.SetVariable("inputText", $inputText)
-
-    $dateScriptBlock = {
-        while ($true) {
-            $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-            Write-Host "Current Date: $date"
-
-            Start-Sleep -Milliseconds 500
-        }
+    while ($true) {
+        $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        Write-Host "Current Date: $date"
+        Start-Sleep -Milliseconds $DelayMilliseconds
     }
-
-    $userInputScriptBlock = {
-        while ($true) {
-            Write-Host "User Input:"
-            Write-Host "-----------------------"
-            Write-Host $using:userInput
-            Write-Host "-----------------------"
-
-            $userInput = Read-Host "Enter something:"
-            $script:global:userInput = $userInput
-
-            Start-Sleep -Milliseconds 100
-        }
-    }
-
-    $dateJob = [powershell]::Create().AddScript($dateScriptBlock).AddArgument($runspace)
-    $userInputJob = [powershell]::Create().AddScript($userInputScriptBlock)
-
-    $runspace.SessionStateProxy.SetVariable("userInputJob", $userInputJob)
-
-    $dateJob.Runspace = $runspace
-    $userInputJob.Runspace = $runspace
-
-    $dateJob.BeginInvoke()
-    $userInputJob.BeginInvoke()
-
-    # Wait for user to exit the script
-    do {
-        Start-Sleep -Seconds 1
-    } while ($true)
 }
 
-# Start the ASCII GUI
-Show-AsciiGui
+function GetUserInput {
+    while ($true) {
+        Write-Host "User Input:"
+        Write-Host "-----------------------"
+        Write-Host $global:userInput
+        Write-Host "-----------------------"
+
+        $userInput = Read-Host "Enter something:"
+        $global:userInput = $userInput
+        Start-Sleep -Milliseconds 100
+    }
+}
+
+# Initialize the userInput variable
+$global:userInput = ""
+
+# Start background jobs
+$jobDate = Start-Job -ScriptBlock ${function:Update-Date} -ArgumentList 500
+$jobUserInput = Start-Job -ScriptBlock ${function:GetUserInput}
+
+# Wait for user to exit the script
+do {
+    Start-Sleep -Seconds 1
+} while ($true)
+
+# Cleanup jobs
+Stop-Job -Job $jobDate
+Stop-Job -Job $jobUserInput
