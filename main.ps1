8380cd0150ce13718d072dbd6df6f31e7ad9922c
Lenanrd Boysen <57866677+LenBoysen@users.noreply.github.com>
2023-11-28 16:33:45 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -71,8 +71,11 @@ do {
             switch ($selectedOption) {
                 #0 {Option1}
                 #1 {Option1}
-                "Option1" {Option1}
-                #[MenuOption]::Option1 { Option1 }
+                #"Option1" {Option1}
+                [MenuOption]::Option1 { Option1 }
+                [string][MenuOption]::Option1 { Option2 }
+                [MenuOption]::Option1.ToString() { Option1 }
+                [string][MenuOption]::Option1.ToString() { Option2 }
                 #[int][MenuOption]::Option1 { Option1 }
                 [int][MenuOption]::Option2 { Option2 }
                 [int][MenuOption]::Exit { break }
