af6b8ea88780b47298ffb4f07e3e2cf25a5118c4
Lenanrd Boysen <57866677+LenBoysen@users.noreply.github.com>
2023-11-28 20:05:54 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,40 +1,58 @@
-function Update-Date {
+Add-Type -AssemblyName PresentationFramework
+
+function Show-AsciiGui {
     param (
-        [int]$DelayMilliseconds
+        [string]$inputText
     )
 
-    while ($true) {
-        $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Write-Host "Current Date: $date"
-        Start-Sleep -Milliseconds $DelayMilliseconds
-    }
+    $initialDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    $xaml = @'
+<Window
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="ASCII GUI" Height="150" Width="400" ResizeMode="NoResize">
+    <Grid>
+        <TextBlock x:Name="DateText" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,10,0,0" TextWrapping="Wrap" Text="{Binding DateText}"/>
+        <TextBlock x:Name="UserInputText" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,50,0,0" TextWrapping="Wrap" Text="{Binding UserInputText}"/>
+    </Grid>
+</Window>
+'@
+
+    $reader = (New-Object System.Xml.XmlNodeReader $xaml)
+    $window = [Windows.Markup.XamlLoader]::Load($reader)
+
+    $dateTextBlock = $window.FindName('DateText')
+    $userInputTextBlock = $window.FindName('UserInputText')
+
+    $runspace = [runspacefactory]::CreateRunspace()
+    $runspace.ApartmentState = "STA"
+    $runspace.ThreadOptions = "ReuseThread"
+    $runspace.Open()
+
+    $powerShell = [PowerShell]::Create()
+    $powerShell.Runspace = $runspace
+
+    $powerShell.AddScript({
+        while ($true) {
+            $script:date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+            $script:userInput = $using:userInput
+
+            $window.Dispatcher.Invoke([Windows.Threading.DispatcherPriority]::Background, [Windows.Threading.DispatcherOperationCallback]{
+                $dateTextBlock.Text = "Current Date: $script:date"
+                $userInputTextBlock.Text = "User Input:`n-----------------------`n$script:userInput`n-----------------------"
+                $null
+            }, $null)
+            
+            Start-Sleep -Milliseconds 500
+        }
+    })
+
+    $runspace.SessionStateProxy.SetVariable("userInput", $inputText)
+    $powerShell.BeginInvoke()
+
+    [Windows.Markup.ComponentDispatcher]::Run()
 }
 
-function GetUserInput {
-    while ($true) {
-        Write-Host "User Input:"
-        Write-Host "-----------------------"
-        Write-Host $global:userInput
-        Write-Host "-----------------------"
-
-        $userInput = Read-Host "Enter something:"
-        $global:userInput = $userInput
-        Start-Sleep -Milliseconds 100
-    }
-}
-
-# Initialize the userInput variable
-$global:userInput = ""
-
-# Start background jobs
-$jobDate = Start-Job -ScriptBlock ${function:Update-Date} -ArgumentList 500
-$jobUserInput = Start-Job -ScriptBlock ${function:GetUserInput}
-
-# Wait for user to exit the script
-do {
-    Start-Sleep -Seconds 1
-} while ($true)
-
-# Cleanup jobs
-Stop-Job -Job $jobDate
-Stop-Job -Job $jobUserInput
+# Start the ASCII GUI
+Show-AsciiGui
